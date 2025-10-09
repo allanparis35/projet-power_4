4940c225d7a0214f16aa275aeab2ebdb99d94c32
allan paris <allanparis8@gmail.com>
2025-10-09 10:38:22 +0200

    affiche board

---
+++ table/table.go
@@ -0,0 +1,37 @@
+package table
+
+
+type GameState struct {
+	Board         [6][7]string
+	CurrentPlayer string
+	Winner        string
+}
+
+var State GameState
+
+func Reset() {
+	State = GameState{
+		Board:         [6][7]string{},
+		CurrentPlayer: "red",
+		Winner:        "",
+	}
+}
+
+func PlacePiece(col int) bool {
+
+	for row := 5; row >= 0; row-- {
+		if State.Board[row][col] == "" {
+			State.Board[row][col] = State.CurrentPlayer
+			return true
+		}
+	}
+	return false
+}
+
+func SwitchPlayer() {
+	if State.CurrentPlayer == "red" {
+		State.CurrentPlayer = "yellow"
+	} else {
+		State.CurrentPlayer = "red"
+	}
+}
