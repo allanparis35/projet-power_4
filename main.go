65349ee4fc82f333ec90fe81edd203a84b6a7e71
allan paris <allanparis8@gmail.com>
2025-10-16 09:16:46 +0200

    main

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
+
 import (
-    "net/http"
+	"net/http"
 
+	"projet-power_4/handlers"
+	"projet-power_4/table"
 )
-func main() {
-    http.HandleFunc("/markdown", GenerateMarkdown)
-    http.Handle("/", http.FileServer(http.Dir("public")))
-    http.ListenAndServe(":8080", nil)
 
-}
-func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+func main() {
+	table.Reset()
+
+	http.HandleFunc("/", handlers.ShowBoard)
+	http.HandleFunc("/play", handlers.PlayMove)
+	http.HandleFunc("/reset", handlers.ResetHandler)
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8080", nil)
 }
