60e852758676e936af83aa278bf6a3aafe2ff00e
bmanao <manao.brunetti@ynov.com>
2025-11-16 02:33:00 +0100

    quick audio fix

--- static/music.js
+++ static/music.js
@@ -1,6 +1,6 @@
 const audio = document.getElementById('game-audio');
 
-// Sauvegarder l'état avant le refresh
+
 window.addEventListener('beforeunload', function() {
     if (!audio.paused) {
         localStorage.setItem('musicPlaying', 'true');
@@ -10,7 +10,7 @@ window.addEventListener('beforeunload', function() {
     }
 });
 
-// Restaurer l'état après le refresh
+
 window.addEventListener('load', function() {
     if (localStorage.getItem('musicPlaying') === 'true') {
         const savedTime = parseFloat(localStorage.getItem('musicTime') || 0);
